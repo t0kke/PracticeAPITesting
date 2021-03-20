b87065c75f0f53fb825f0796f4a56e11c0a0bc6c
German <k4tger@gmail.com>
2021-03-20 23:44:42 +0300

    Added test create new user

--- src/test/java/api/ApiTests.java
+++ src/test/java/api/ApiTests.java
@@ -3,11 +3,15 @@ package api;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static java.net.HttpURLConnection.HTTP_CREATED;
 import static java.net.HttpURLConnection.HTTP_OK;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class ApiTests {
 
@@ -18,6 +22,7 @@ public class ApiTests {
                 .contentType(ContentType.JSON);
     }
 
+    @DisplayName("Сверяем Json схему сервера с эталонной")
     @Test
     void checkJsonSchemaOfSingleUser() {
         given()
@@ -26,6 +31,21 @@ public class ApiTests {
                 .then()
                 .statusCode(HTTP_OK)
                 .body(matchesJsonSchemaInClasspath("singleUser.json"))
+                .log().body();
+
+    }
+
+    @DisplayName("Создаем нового пользователя")
+    @Test
+    void createNewUser() {
+        given()
+                .body("{ \"name\": \"George Clooney\", \"job\": \"actor\" }")
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(HTTP_CREATED)
+                .body("name", is("George Clooney"))
+                .body("id", is(notNullValue()))
                 .log().body();
 
     }
